dbbe6c1814b7dfca04bb8a5a24776e2f2c85d96e
gomes-tkd <jgomestkd@gmail.com>
2024-01-11 21:42:08 -0300

    Adicionado JS final e CSS final na pagina

--- js/script.js
+++ js/script.js
@@ -24,3 +24,48 @@ function ativarParametrosProdutoSelecionado(parametro) {
 }
 
 paramentros.forEach(ativarParametrosProdutoSelecionado);
+
+// Ativa as perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPerguntaClick(e) {
+  const pergunta = e.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function ativaEventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPerguntaClick);
+}
+
+perguntas.forEach(ativaEventoPerguntas);
+
+// Galeria de bicicletas --> página de cada bicicleta única
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(e) {
+  const img = e.currentTarget;
+  const media = matchMedia("(max-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+} else {
+  
+}
